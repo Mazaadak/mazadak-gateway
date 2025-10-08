75874d597447e731b411afdfac80c2b8b023c6ac
Hassan Ali <162273873+HassanIsmail16@users.noreply.github.com>
2025-10-08 03:26:36 +0300

    feat(Security): added UserIdAttachmentFilter

---
+++ src/main/java/com/mazadak/gateway/filters/UserIdAttachmentFilter.java
@@ -0,0 +1,35 @@
+package com.mazadak.gateway.filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class UserIdAttachmentFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String token = exchange.getRequest().getHeaders().getFirst("Authorization");
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+            String userId = decodeJwt(token);
+
+            exchange.getRequest()
+                    .mutate()
+                    .header("X-User-ID", userId)
+                    .build();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+
+    private String decodeJwt(String token) {
+        return "1"; // TODO: implement JWT decoding to extract user ID
+    }
+}
