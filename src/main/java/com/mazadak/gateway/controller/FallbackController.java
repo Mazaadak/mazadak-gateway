d2afcdb11c168f33b414cb4243402c5e955b66e8
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2025-11-01 22:54:30 +0200

    feat: implement fallback endpoint

---
+++ src/main/java/com/mazadak/gateway/controller/FallbackController.java
@@ -0,0 +1,29 @@
+package com.mazadak.gateway.controller;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.net.URI;
+import java.time.Instant;
+
+@RestController
+@RequestMapping("/fallback")
+public class FallbackController {
+
+    @GetMapping
+    public ProblemDetail defaultFallback(ServerWebExchange exchange) {
+        String requestUri = exchange.getRequest().getURI().toString();
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.SERVICE_UNAVAILABLE);
+        problemDetail.setTitle("Service Unavailable");
+        problemDetail.setDetail("A downstream service is temporarily unavailable. Please try again later.");
+        problemDetail.setProperty("timestamp", Instant.now());
+        problemDetail.setInstance(URI.create(requestUri));
+        return problemDetail;
+    }
+
+}
