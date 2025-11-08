76532e9ba3ae58df1331d311d79c7a6367f409ca
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2025-11-08 11:18:26 +0200

    feat: add common-module

--- src/main/java/com/mazadak/gateway/GatewayApplication.java
+++ src/main/java/com/mazadak/gateway/GatewayApplication.java
@@ -2,8 +2,10 @@ package com.mazadak.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.mazadak.gateway", "com.mazadak.common"})
 public class GatewayApplication {
 
     public static void main(String[] args) {
